73d733721ffe255f17ec2c849a75ee18fbd57bab
pappyN4 <99154261+pappyN4@users.noreply.github.com>
2025-01-26 14:28:28 -0500

    Update ntoskrnl_redirects.h V14
    v14, added PoFxSetDeviceIdleTimeout for RtsUer.sys 10.0.26100.31288 x64 testing

--- ntoskrnl_redirects.h
+++ ntoskrnl_redirects.h
@@ -13320,6 +13320,7 @@ k8_win8(   	PoFxActivateComponent,				12)
 k8_win8(   	PoFxIdleComponent,					12)
 k8_win8(   	MmMapIoSpaceEx,					    16)
 k8_win8(    memcmp,                             12)
+k8_win8(   	PoFxSetDeviceIdleTimeout,			12)
 
 #endif  // < Win8 x32/x64 Extender
 
